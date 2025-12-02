4008ab587e6fbff85ece2e4440ed6f94aec43214
vinicius almeida <136178252+vinexfg@users.noreply.github.com>
2025-12-02 20:04:52 -0300

    aaaaa

---
+++ aula02/script.js
@@ -0,0 +1,52 @@
+from prompt import prompt
+
+// let x = 16
+
+// if (x <= 16){
+//     console.log(' nao pode voltar ')
+// }
+
+
+
+//Notas do alubos full_stack
+
+
+// const nota = 7
+
+
+// if (nota >=8){
+//     console.log('aprovado')
+// }else if(nota >= 7){
+//     console.log('Aprovado')
+// }else{
+//     console.log(' Reprovado')
+// }
+
+                                                                                                                                                                                                 
+// SISTEMA DE MEDIA
+
+
+
+// const nota = 7;
+
+// let nota1 = 4
+// let nota2 = 8
+
+
+// media = (nota1 + nota2)/2
+
+// if(media >=8){
+//     console.log('Aprovado com louvor');
+    
+// }else if(media >=7){
+//     console.log(' Aprovado')
+// }else if(media >=3 && media >=6 ){
+//     console.log('Recuperacao');
+// }else{
+//     console.log('reprovado')
+// }
+
+6
+
+
+
