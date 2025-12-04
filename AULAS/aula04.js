debb9e0ce914b19e67714afb52b83529cdc9c6bd
vinicius almeida <136178252+vinexfg@users.noreply.github.com>
2025-12-04 19:43:57 -0300

    function

--- AULAS/aula04.js
+++ AULAS/aula04.js
@@ -1,5 +1,94 @@
 
 
-function falaOi(){
-    console.log(oi);
+// function falaOi(){
+//     console.log('oi');
+//     }
+
+
+
+// falaOi()
+
+
+
+// const souUmDado = function(){
+//     console.log('Sou um dado')
+// }
+
+
+// souUmDado()
+
+// 
+
+
+// const melhorDoMundo = () =>{
+//     console.log(' alor')
+// }
+
+
+// melhorDoMundo()
+
+
+
+
+
+// const somar = function (a, b){
+//     return 2 + 4
+// }
+
+
+
+// console.log(somar())
+
+
+
+// const mutiplicar = function(a,b){
+//     return a * b  
+// }
+
+// console.log(mutiplicar(8,9))
+
+
+
+// const quadrado = (x) => x**2;
+
+
+// console.log(quadrado(5));
+
+
+
+// const raiz = (x) => (x**(1/2))
+
+// console.log(raiz(64))
+
+
+function divisao(valor){
+
+    if(valor == 0 ){
+        console.log(' seu numero e zero nao pode ser dividido' );
+        
+    }else{
+        resultado = (valor/2)
+        return console.log(resultado);
+        
     }
+}
+
+divisao(5)
+
+
+function Verificar(numb1, numb2){
+    return (numb1 == 0  || numb2 ==0 ? "Nao pode dividir por zero ": numb1/numb2)
+}
+
+
+console.log(Verificar(5, ));
+
+
+
+
+function dividir(a,b){
+    if(b=== 0) return ' Divisao por zero 0'
+    return a/b
+}
+
+console.log(dividir(10.00));
