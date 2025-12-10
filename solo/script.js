3a8b03b07e7d188ccc86e9f6c37788b9014559b2
vinexfg <vinysilvaa20@gmail.com>
2025-12-10 09:09:20 -0300

    a

---
+++ solo/script.js
@@ -0,0 +1,30 @@
+// const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 10];
+
+// const dobrados = numeros.map((n) => n * 2);
+
+// console.log(dobrados);
+
+// const idade = [29, 24, 21, 15];
+
+// const adultos = idade.filter((i) => i <= 18);
+// console.log(adultos);
+
+// const pessoas = ["vinicius", "alvaro", "marquinhos do grau", "alvaro"];
+
+// let achado = pessoas.find((p) => p === "alvaro");
+// console.log(achado);
+
+// const numeros = [5, 10, 15];
+
+// let indice = numeros.findIndex((n) => n === 10);
+// console.log(indice);
+
+// const notas = [3, 5, 7];
+
+// const temAprovado = notas.some((n) => n <= 6);
+// console.log(temAprovado);       //True se algum elemento for TRUEEEEEE
+
+const notas = [9, 6, 7];
+
+let todosAprovados = notas.every((n) => n >= 6);
+console.log(todosAprovados);
